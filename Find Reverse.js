7773d653fde59646f619b6c1537d7d56bec169dc
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-10-16 15:13:03 +0300

    Create First Reverse

---
+++ Find Reverse.js
@@ -0,0 +1,16 @@
+/*
+Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+Examples
+Input: "coderbyte"
+Output: etybredoc
+Input: "I Love Code"
+Output: edoC evoL I
+*/
+
+function FirstReverse(str) { 
+    var newStr = "", len = str.length;
+    while(len--) newStr += str[len];
+    return newStr; 
+}
+     
+console.log(FirstReverse(readline()));
