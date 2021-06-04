10e49024cafc40291cdc09a72e407e2c50eac684
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2021-06-04 12:51:18 +0300

    Update and rename Unknown1 to Unknown1.js

--- Unknown1.js
+++ Unknown1.js
@@ -1,3 +1,13 @@
+/*
+fonksiyon(A, F, N);
+A dizini, F adet olmalıydı ama sayılar kayboldu. Bu sayılar 7 sayısından küçük 1 sayısından büyük olduğu bilinmektedir. Tüm sayıların ortalaması N olması için kayıp sayılar bulunabilir mi? Bulunursa bu sayılar neler olmalı(1 örnek yeterli), bulunmazsa da 0 diyebilirsiniz.
+Örnekler:
+
+Given A = [3, 2, 4, 3], F = 2, M = 4, your function should return [6, 6]. The arithmetic mean of all the rolls Is (3 + 2 + 4 + 3 + 6 + 6) / 6 = 24 / 6 = 4.
+Given A = [1, 5, 6], F = 4, M = 3. your function may retum [2. 1, 2, 4] or [6, 1, 1, 1] (among others).
+Given A = [1, 2, 3, 4], F = 4, M = 6, your function should return [0]. It is not possible to obtain such a mean. 4. Given A = [6,11 F = 1, M = 1, your function should return [0]. It Is not possible to obtain such a mean.
+*/
+
 function solution(A, F, M) {
     maxValidDice = 6;
     lenOfA = A.length;
